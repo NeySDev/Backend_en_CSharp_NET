fd45c8d390675f8f838585ba62a49d0efc964728
Nelson Ysain Ortiz Leal <nelsonortizleal@gmail.com>
2025-02-23 00:11:26 -0600

    Feat: PeopleController con metodo GetPeoples

---
+++ Backend/Backend/Controllers/PeopleController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.NetworkInformation;
+
+namespace Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PeopleController : ControllerBase
+    {
+        [HttpGet(Name = "All")]
+        public List<People> GetPeoples() => Repository.People;
+    }
+
+    #region DATA
+    public static class Repository
+    {
+        // Almacenamiento en memoria de personas
+        public static List<People> People = new List<People>()
+        {
+            new People()
+            {
+                Id = 1,
+                Name = "Nelson",
+                Age = 26
+            },
+            new People()
+            {
+                Id = 2,
+                Name = "Alexa",
+                Age = 30
+            },
+            new People()
+            {
+                Id = 3,
+                Name = "Viqui",
+                Age = 33
+            },
+        };
+    }
+    #endregion
+
+    #region MODELS
+    public class People
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+    #endregion
+}
