8ef49b0002e221614b315b8165fc15d4b78adc00
Nelson Ysain Ortiz Leal <nelsonortizleal@gmail.com>
2025-03-08 01:41:58 -0600

    Feact: Creacion de método sincrono

---
+++ Backend/Backend/Controllers/SomeController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SomeController : ControllerBase
+    {
+        [HttpGet("sync")]
+        public IActionResult GetSync()
+        {
+            Stopwatch sw = Stopwatch.StartNew();
+
+            Thread.Sleep(1000);
+            Console.WriteLine("Conectando a la BD.");
+
+            Console.WriteLine("Haciendo algo intermedio.");
+
+            Thread.Sleep(1000);
+            Console.WriteLine("Enviando correo.");
+
+            Console.WriteLine("Fin del programa.");
+
+            sw.Stop();
+            Console.WriteLine("Tiempo transcurrido: " + sw.Elapsed);
+
+            return Ok();
+        }
+    }
+}
